b545c1db79fc841685b98457bcc0f116cbbaed85
Khaled Adel <75905785+KhaledAdelM@users.noreply.github.com>
2022-05-21 18:46:29 +0200

    Add files via upload

---
+++ Com/Com/Models/Script.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using Com.Controllers;
+
+namespace Com.Models
+{
+    public class Script
+    {
+        public string MyText { get; set; }
+        public string PaperFile { get; set; }
+        public HttpPostedFileBase FilePost { get; set; }
+
+    }
+}
